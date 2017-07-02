851fcb01bd1cc001b14920830038bbc72d50a34b
ToshikiHennig <toshiki.hennig@gmail.com>
2017-07-02 22:17:13 +0200

    Json-Object pushed to server

--- app/src/main/java/cakhen/piclive/ImageActivity.java
+++ app/src/main/java/cakhen/piclive/ImageActivity.java
@@ -33,6 +33,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -50,7 +51,7 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class ImageActivity extends AppCompatActivity {
-    public static final MediaType FORM = MediaType.parse("application/json");
+    public static final MediaType FORM = MediaType.parse("application/json; charset=utf-8");
 
     ImageView imageView;
     Drawable d;
@@ -58,6 +59,7 @@ public class ImageActivity extends AppCompatActivity {
     Button post;
     ProgressBar progressBar;
     String path;
+    String base64;
 
     private OkHttpClient client;
     private LocationDTO MyLocationDTO;
@@ -79,7 +81,13 @@ public class ImageActivity extends AppCompatActivity {
         showImage(intent.getStringExtra("pathName"));
         this.path = intent.getStringExtra("pathName");
         picName = (EditText) findViewById(R.id.picName);
-
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                base64 = createByteFromImage();
+            }
+        };
+        thread.start();
         client = new OkHttpClient.Builder()
                 .connectTimeout(500, TimeUnit.SECONDS)
                 .writeTimeout(500, TimeUnit.SECONDS)
@@ -90,6 +98,7 @@ public class ImageActivity extends AppCompatActivity {
 
     private void Home(){
         Intent i_login = new Intent(getApplicationContext(), PicLiveActivity.class);
+        i_login.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(i_login);
         finish();
     }
@@ -101,15 +110,26 @@ public class ImageActivity extends AppCompatActivity {
 
         }
 
-    public void postPicture(View view) {
-        findViewById(R.id.overlayimage).setVisibility(View.VISIBLE);
-        progressBar.setVisibility(View.VISIBLE);
-        progressBar.bringToFront();
-        post.setEnabled(false);
-        picName.setEnabled(false);
+    private void backToPicture(){
+        findViewById(R.id.overlayimage).setVisibility(View.GONE);
+        findViewById(R.id.textPosting).setVisibility(View.GONE);
+        progressBar.setVisibility(View.GONE);
+        post.setEnabled(true);
+        picName.setEnabled(true);
+    }
 
+    public void postPicture(View view) {
 
         if(picName.getText().length() >= 6){
+            findViewById(R.id.overlayimage).setVisibility(View.VISIBLE);
+            findViewById(R.id.overlayimage).bringToFront();
+            findViewById(R.id.textPosting).setVisibility(View.VISIBLE);
+            findViewById(R.id.textPosting).bringToFront();
+            progressBar.setVisibility(View.VISIBLE);
+            progressBar.bringToFront();
+            post.setEnabled(false);
+            picName.setEnabled(false);
+            Log.d("SIZE OF Base64", base64.length() + "");
             new GetLocationAsyncTask().execute();
         }
         else{
@@ -125,24 +145,33 @@ public class ImageActivity extends AppCompatActivity {
         bitmap.compress(Bitmap.CompressFormat.JPEG,100, baos);
         //Log.d("sizebaos: ", baos.size() + "");
         byte[] b = baos.toByteArray();
-        //Log.d("bytesadsadas: ", b.length + "");
+        //
         //Log.d("bytetoString: ", b + "");
         String temp=Base64.encodeToString(baos.toByteArray(), Base64.NO_WRAP);
-        //System.out.println(temp.length());
+        Log.d("bytesadsadas: ", temp.length() + "");
         return temp;
     }
 
     private Response PostPicture(PictureUploadDTO Upload){
         // Create a new HttpClient and Post Header
+        Log.d("NAME", Upload.Name);
+        Log.d("City", Upload.City);
+        Log.d("Lat", Upload.Lat + "");
+        Log.d("Lng", Upload.Lng + "");
+        Log.d("IMAGE", Upload.Image);
         JSONObject jsonObject = new JSONObject();
         try {
-            jsonObject.put("City", Upload.City);
-            jsonObject.put("Image", Upload.Image);
-            jsonObject.put("Lat", Upload.Lat);
-            jsonObject.put("Lng", Upload.Lng);
-
-            RequestBody body = RequestBody.create(FORM, String.valueOf(jsonObject));
+            jsonObject.put("name", Upload.Name);
+            jsonObject.put("image",base64);
+            jsonObject.put("lng", String.valueOf(Upload.Lng));
+            jsonObject.put("lat", String.valueOf(Upload.Lat));
+            jsonObject.put("city", Upload.City);
+            Log.d("Json-Object", String.valueOf(jsonObject));
+            Log.d("Correct Base64", base64);
+            RequestBody body = RequestBody.create(FORM, "" +"{Name:" + Upload.Name + " , Image:" + jsonObject.get("image") +", Lng: "+ Upload.Lng +", Lat: "+ Upload.Lat +", City:"+ Upload.City +"}");
+            Log.d("Requestbody", jsonObject.get("image") +"");
             Request request = new Request.Builder()
+                    .header("Authorization", TokenSaver.getToken(getApplicationContext()))
                     .url(Globals.API + "Pictures")
                     .post(body)
                     .build();
@@ -164,15 +193,39 @@ public class ImageActivity extends AppCompatActivity {
             if(http_response.code() == 200){
                 Toast.makeText(getApplicationContext(), "Upload Successfull", Toast.LENGTH_SHORT).show();
                 Home();
+                backToPicture();
             }
             else if(http_response.code() == 400){
+                backToPicture();
                 Toast.makeText(getApplicationContext(), "Not all required Data inputs sent", Toast.LENGTH_SHORT).show();
+                try {
+                    Log.d("HTTP_RESPONSE", http_response.body().string().toString());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
             else{
+                backToPicture();
                 Toast.makeText(getApplicationContext(), "FAIL! Something unusual happened", Toast.LENGTH_SHORT).show();
             }
         }
 
+    }
+
+    public void generateNoteOnSD(Context context, String sFileName, String sBody) {
+        try {
+            File root = new File(Environment.getExternalStorageDirectory(), "Notes");
+            if (!root.exists()) {
+                root.mkdirs();
+            }
+            File gpxfile = new File(root, sFileName);
+            FileWriter writer = new FileWriter(gpxfile);
+            writer.append(sBody);
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private class GetLocationAsyncTask extends AsyncTask<Void, Void, Location> {
@@ -188,7 +241,7 @@ public class ImageActivity extends AppCompatActivity {
                 new UploadPictureAsyncTask().execute(
                         new PictureUploadDTO(
                                 picName.getText().toString(),
-                                createByteFromImage(),
+                                base64,
                                 MyLocationDTO.Lng,
                                 MyLocationDTO.Lat,
                                 MyLocationDTO.City
@@ -200,6 +253,13 @@ public class ImageActivity extends AppCompatActivity {
             }
         }
 
+    }
+
+    public void cancelUpload(View view){
+        base64 = null;
+        Intent i = new Intent(ImageActivity.this, CameraActivity.class);
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(i);
     }
 
 
